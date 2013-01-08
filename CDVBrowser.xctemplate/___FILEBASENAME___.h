a9f6c9bcd6f2b0eb52409a34d414cc50730ca73f
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-01-07 23:59:25 -0500

    modified:   CDVBrowser.xctemplate/___FILEBASENAME___.h 	modified:   CDVBrowser.xctemplate/___FILEBASENAME___.js 	modified:   CDVBrowser.xctemplate/___FILEBASENAME___.m 	modified:   CDVBrowser.xctemplate/___FILEBASENAME___ViewController.h 	modified:   CDVBrowser.xctemplate/___FILEBASENAME___ViewController.m 	modified:   README.md

--- CDVBrowser.xctemplate/___FILEBASENAME___.h
+++ CDVBrowser.xctemplate/___FILEBASENAME___.h
@@ -1,15 +1,19 @@
 //  Created by Jesse MacFadyen on 10-05-29.
 //  Copyright 2010 Nitobi. All rights reserved.
 //  Copyright 2012, Randy McMillan
+//
+//  ___FILEBASENAME___
+//  CDVEmailComposer Template Created Jan 7 2013
+//  Copyright 2013 @RandyMcMillan
 
 #import <Cordova/CDVPlugin.h>
 #import "___FILEBASENAME___ViewController.h"
 
 @interface ___FILEBASENAME___ : CDVPlugin <___FILEBASENAME___Delegate>{}
 
-@property (nonatomic, strong) ___FILEBASENAME___ViewController* ___FILEBASENAME___;
+@property (nonatomic, strong) ___FILEBASENAME___ViewController *___FILEBASENAME___;
 
-- (void)showWebPage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
-- (void)onChildLocationChange:(NSString*)newLoc;
+- (void)showWebPage:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
+- (void)onChildLocationChange:(NSString *)newLoc;
 
 @end
--- CDVBrowser.xctemplate/___FILEBASENAME___ViewController.h
+++ CDVBrowser.xctemplate/___FILEBASENAME___ViewController.h
@@ -1,6 +1,9 @@
 //  Created by Jesse MacFadyen on 10-05-29.
 //  Copyright 2010 Nitobi. All rights reserved.
-//  Copyright 2012, Randy McMillan
+//
+//  ___FILEBASENAME___
+//  CDVEmailComposer Template Created Jan 7 2013
+//  Copyright 2013 @RandyMcMillan
 
 #import <UIKit/UIKit.h>
 
@@ -12,7 +15,7 @@
  *  Discussion:
  *    Invoked when a new page has loaded
  */
-- (void)onChildLocationChange:(NSString*)newLoc;
+- (void)onChildLocationChange:(NSString *)newLoc;
 - (void)onOpenInSafari;
 - (void)onClose;
 
@@ -28,27 +31,27 @@
 
 @interface ___FILEBASENAME___ViewController : UIViewController <UIWebViewDelegate>{}
 
-@property (nonatomic, strong) IBOutlet UIWebView* webView;
-@property (nonatomic, strong) IBOutlet UIBarButtonItem* closeBtn;
-@property (nonatomic, strong) IBOutlet UIBarButtonItem* refreshBtn;
-@property (nonatomic, strong) IBOutlet UILabel* addressLabel;
-@property (nonatomic, strong) IBOutlet UIBarButtonItem* backBtn;
-@property (nonatomic, strong) IBOutlet UIBarButtonItem* fwdBtn;
-@property (nonatomic, strong) IBOutlet UIBarButtonItem* safariBtn;
-@property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
+@property (nonatomic, strong) IBOutlet UIWebView				*webView;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem			*closeBtn;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem			*refreshBtn;
+@property (nonatomic, strong) IBOutlet UILabel					*addressLabel;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem			*backBtn;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem			*fwdBtn;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem			*safariBtn;
+@property (nonatomic, strong) IBOutlet UIActivityIndicatorView	*spinner;
 
 // unsafe_unretained is equivalent to assign - used to prevent retain cycles in the two properties below
-@property (nonatomic, unsafe_unretained) id <___FILEBASENAME___Delegate> delegate;
-@property (nonatomic, unsafe_unretained) id orientationDelegate;
+@property (nonatomic, unsafe_unretained) id <___FILEBASENAME___Delegate>	delegate;
+@property (nonatomic, unsafe_unretained) id									orientationDelegate;
 
-@property (copy) NSString* imageURL;
-@property (assign) BOOL isImage;
-@property (assign) BOOL scaleEnabled;
+@property (copy) NSString	*imageURL;
+@property (assign) BOOL		isImage;
+@property (assign) BOOL		scaleEnabled;
 
-- (___FILEBASENAME___ViewController*)initWithScale:(BOOL)enabled;
+- (___FILEBASENAME___ViewController *)initWithScale:(BOOL)enabled;
 - (IBAction)onDoneButtonPress:(id)sender;
 - (IBAction)onSafariButtonPress:(id)sender;
-- (void)loadURL:(NSString*)url;
+- (void)loadURL:(NSString *)url;
 - (void)closeBrowser;
 
 @end
